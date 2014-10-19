961db5786289de33df9831448bb122dba52dfb95
Alexander Lisachenko <lisachenko.it@gmail.com>
2014-10-19 22:16:21 +0400

    Site updated at 2014-10-19 18:16:21 UTC

--- downloads/code/privileged-advices/TestPrivileged.php
+++ downloads/code/privileged-advices/TestPrivileged.php
@@ -14,3 +14,32 @@ class TestPrivileged
         echo "TestPrivileged.method1";
     }
 }
+
+
+class CachingProxy
+{
+    private $cache = null;
+    private $instance = null;
+
+    public function __construct(Memcache $cache, $instance)
+    {
+        $this->cache    = $cache;
+        $this->instance = $instance;
+    }
+
+    public function __call($method, $arguments)
+    {
+        if (substr($method, 0, 3) !== 'get') {
+            $result = call_user_func_array($method, $arguments);
+        } else {
+            $uniqueId = $method . serialize($arguments);
+            $result = $this->cache->get($uniqueId);
+            if ($result === false) {
+                $result = call_user_func_array($method, $arguments);
+                $this->cache->set($uniqueId, $result);
+            }
+        }
+
+        return $result;
+    }
+}
